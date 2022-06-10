c5d1386858cb1796ae5b0c5372c7bccca24a56f7
Al-Ahmadgaid Asaad <alahmadgaid@gmail.com>
2022-06-10 20:47:33 +0800

    update docs

--- docs/make.jl
+++ docs/make.jl
@@ -2,14 +2,20 @@ using Documenter
 using Bagyo
 
 makedocs(
-    sitename = "Bagyo",
-    format = Documenter.HTML(),
-    modules = [Bagyo]
+    sitename = "Bagyo.jl",
+    # format = Documenter.HTML(
+    #     assets = ["assets/favicon.ico"]
+    # ),
+    authors = "Al-Ahmadgaid B. Asaad",
+    modules = [Bagyo],
+    pages = [
+        "Home" => "index.md"
+    ]
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/alstat/Bagyo.jl.git"
+)
