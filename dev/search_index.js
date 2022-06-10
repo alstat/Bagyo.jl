ec3c0ece74b1f2c808e48e91d64e210d0d1cf716
Documenter.jl <documenter@juliadocs.github.io>
2022-06-10 12:48:53 +0000

    build based on c5d1386

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Bagyo.jl","page":"Home","title":"Bagyo.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Bagyo.jl","category":"page"}]
+}
